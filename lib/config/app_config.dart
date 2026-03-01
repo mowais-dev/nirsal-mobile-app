a924125f19b265ec040083d766403c61732b7797
Muhammad Owais <owais.gosha@gmail.com>
2026-03-01 21:08:26 +0500

    Final changes

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,7 +1,4 @@
 class AppLocalConfig {
-  static const String apiBaseUrl = 'https://stage.armsserv.com/api/v1';
-  static const String appUrl = 'http://192.168.8.143:8000';
-
-  // static const String apiBaseUrl = 'https://app.washlab.lk/api/v1';
-  // static const String appUrl = 'https://app.washlab.lk';
+  static const String apiBaseUrl = 'http://localhost:8000/api/v1';
+  static const String appUrl = 'http://localhost:8000';
 }
